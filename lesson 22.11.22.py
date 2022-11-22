3f9f4e855de3016ad379248d32b27f8702b3a3bd
tokmarkovka <tokmarkovka@gmail.com>
2022-11-22 21:51:32 +0200

    variables and types of variables

---
+++ lesson 22.11.22.py
@@ -0,0 +1,16 @@
+print('hello')
+
+# variable = None       если надо назначить какой-то тип
+
+# variable = int()      целые числа
+# variable = float()    дробные числа
+# variable = str()      строки
+# variable = bool()     логические данные (True/False)
+
+# Ctrl+Alt+L            автоматические форматирвание по PEP-8
+
+variable = 7
+print(variable)
+variable = 'this is string'
+print(variable)
+
