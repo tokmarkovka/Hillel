586f80644b263d925b6986213a384a4fcbd7fa3e
tokmarkovka <tokmarkovka@gmail.com>
2022-11-22 22:11:12 +0200

    strings and escape sequences (последовательности)

--- lesson 22.11.22.py
+++ lesson 22.11.22.py
@@ -1,16 +1,25 @@
-print('hello')
+# print('hello')
+#
+# # variable = None       если надо назначить какой-то тип
+#
+# # variable = int()      целые числа
+# # variable = float()    дробные числа
+# # variable = str()      строки
+# # variable = bool()     логические данные (True/False)
+#
+# # Ctrl+Alt+L            автоматические форматирвание по PEP-8
+#
+# variable = 7
+# print(variable)
+# variable = 'this is string'
+# print(variable)
 
-# variable = None       если надо назначить какой-то тип
-
-# variable = int()      целые числа
-# variable = float()    дробные числа
-# variable = str()      строки
-# variable = bool()     логические данные (True/False)
-
-# Ctrl+Alt+L            автоматические форматирвание по PEP-8
-
-variable = 7
-print(variable)
-variable = 'this is string'
-print(variable)
+some_string = '"my awesome string"'
+print(some_string, end=' ')
+some_string1 = "'my awesome string'"
+print(some_string1)
+some_string2 = '"my awesome\' string"'
+print(some_string2)
+some_string3 = '"my\n awe\tsome\b string"'
+print(some_string3)
 
