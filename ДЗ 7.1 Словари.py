94f7366264a261ddee76da923b2d3ecc85a92922
tokmarkovka <tokmarkovka@gmail.com>
2023-01-16 19:15:27 +0200

    homework 7 30/12/22

--- ДЗ 7.1 Словари.py
+++ ДЗ 7.1 Словари.py
@@ -29,52 +29,39 @@ print('Список самых молодых: ', list_of_youngest_people)
 # б) Создать список и поместить туда самое длинное имя.
 # Если длина имени совпадает - поместить все такие имена.
 
-list_of_longest_names = []
-longest_name = 0
-for l_name in persons:
-    if longest_name < len(l_name['name']):
-        longest_name = len(l_name['name'])
-for l_name in persons:
-    if longest_name == len(l_name['name']):
-        list_of_longest_names.append(l_name['name'])
+# при помощи циклов:
 
-print('Максимальная длина имени: ', longest_name)
-print('Список самых длинных имен: ', list_of_longest_names)
+# list_of_longest_names = []
+# longest_name = 0
+# for l_name in persons:
+#     if longest_name < len(l_name['name']):
+#         longest_name = len(l_name['name'])
+# for l_name in persons:
+#     if longest_name == len(l_name['name']):
+#         list_of_longest_names.append(l_name['name'])
 
-# в пайтон стайле не получилось
-
-# max_name_len = max(age['age'] for age in persons)
-# print('Максимальная длина имени: ', max_name_len)
-# list_of_longest_names = [age['name'] for age in persons if age['age'] == max_name_len]
+# print('Максимальная длина имени: ', longest_name)
 # print('Список самых длинных имен: ', list_of_longest_names)
+
+# в пайтон стайле:
+
+max_name_len = max(len(person['name']) for person in persons)
+print('Максимальная длина имени: ', max_name_len)
+list_of_longest_names = [person['name'] for person in persons if len(person['name']) == max_name_len]
+print('Список самых длинных имен: ', list_of_longest_names)
 
 # в) Посчитать среднее количество лет всех людей из начального списка.
 
-average_age_list = []
-for age in persons:
-    if age in persons:
-        average_age_list.append(age['age'])
-print('Список всех возрастов: ', average_age_list)
-print('Cреднее количество лет всех людей: ', sum(average_age_list)/len(average_age_list))
-print('Cреднее количество полных лет всех людей: ', int(sum(average_age_list)/len(average_age_list)))
-
-
-
+# average_age_list = []
 # for age in persons:
-#     average_number_of_years = sum(persons['age'])
+#     if age in persons:
+#         average_age_list.append(age['age'])
+# print('Список всех возрастов: ', average_age_list)
+# print('Cреднее количество лет всех людей: ', sum(average_age_list)/len(average_age_list))
+# print('Cреднее количество полных лет всех людей: ', int(sum(average_age_list)/len(average_age_list)))
 
-# print(*persons)
-# dict_persons = dict(persons)
-#
-# print('dict_persons', dict_persons['name'])
-# list_with_youngest_person = []
-# print(persons[-1])
-# print(*persons)
-# for name in persons:
-#     print(age.values(), end=' ')
-#
-# # list_with_youngest_person.append(persons[])
-#
-# list_with_longest_name = []
-#
-# average_age = (sum(persons['age'] for persons))
+# в пайтон стайле:
+
+print('Cреднее количество лет всех людей: ', sum(person['age'] for person in persons)/len(persons))
+print('Cреднее количество полных лет всех людей: ', int(sum(person['age'] for person in persons)/len(persons)))
+
